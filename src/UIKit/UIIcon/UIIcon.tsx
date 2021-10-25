2c9ad035f6e96ecdd46e2429b3289f6d1794c9d8
Samuel Oyediran <samuel.oyediran00@gmail.com>
2021-10-25 14:32:08 -0400

    completed product checkout page

--- src/UIKit/UIIcon/UIIcon.tsx
+++ src/UIKit/UIIcon/UIIcon.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ClassNames from 'classnames';
 import { IconType } from '../../utils/icons';
 import UILink from '../UILink/UILink';
-import { RouteType } from '../../routes/utils';
+import { LinkPath, RouteType } from '../../routes/utils';
 import classNames from 'classnames';
 import './UIIcon.scss';
 
@@ -10,11 +10,13 @@ interface UIIconProps {
   iconClassName?: string;
   containerClassName?: string;
   iconType?: IconType;
-  linkTo: string;
+  linkTo?: LinkPath;
   iconText?: string;
-  linkType: RouteType;
+  linkType?: RouteType;
   iconContainerClassName?: string;
   iconTextClassName?: string;
+  onClickIcon?: () => void;
+  hasRoute?: boolean;
 }
 
 const UIIcon: React.FC<UIIconProps> = (props) => {
@@ -23,14 +25,42 @@ const UIIcon: React.FC<UIIconProps> = (props) => {
     iconClassName,
     iconType,
     linkTo,
-    linkType,
+    linkType = RouteType.COMMERCE,
     iconContainerClassName,
     iconText,
     iconTextClassName,
+    hasRoute = true,
   } = props;
   return (
     <div className={ClassNames('icon__container', containerClassName)}>
-      <UILink linkTo={linkTo} isActive={false} linkType={linkType}>
+      {hasRoute ? (
+        <UILink linkTo={linkTo} isActive={false} linkType={linkType}>
+          <div
+            className={ClassNames(
+              'icon__container--content',
+              iconContainerClassName
+            )}
+          >
+            {iconText && (
+              <p
+                className={classNames(
+                  'icon__container--content__text',
+                  iconTextClassName
+                )}
+              >
+                {iconText}
+              </p>
+            )}
+            <i
+              className={ClassNames(
+                'icon__container--content__social-icon',
+                iconType,
+                iconClassName
+              )}
+            ></i>
+          </div>
+        </UILink>
+      ) : (
         <div
           className={ClassNames(
             'icon__container--content',
@@ -55,7 +85,7 @@ const UIIcon: React.FC<UIIconProps> = (props) => {
             )}
           ></i>
         </div>
-      </UILink>
+      )}
     </div>
   );
 };
---
+++ src/components/Products/ProductCategories/ProductCheckout/ProductCheckout.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import UIButton from '../../../../UIKit/UIButton/UIButton';
+import UIIcon from '../../../../UIKit/UIIcon/UIIcon';
+import { IconType } from '../../../../utils/icons';
+import { ProductType } from '../../utils';
+
+interface ProductCheckoutProps {
+  productName?: string;
+  productPrice?: string;
+  productType?: ProductType;
+  productColor?: string;
+}
+
+const ProductCheckout: React.FC<ProductCheckoutProps> = (props) => {
+  const { productName, productPrice, productType, productColor } = props;
+  return (
+    <div className='flex'>
+      <div className='flex justify-center items-center'>
+        <h3>{productName}</h3>
+        <p>{`Price: ${productPrice}`}</p>
+      </div>
+      <div className='flex justify-center items-center'>
+        <p>{`Color: ${productColor}`}</p>
+        {productType ? (
+          <div className='flex justify-center items-center'>
+            <p>S</p>
+            <p>M</p>
+            <p>L</p>
+            <p>XL</p>
+          </div>
+        ) : (
+          <div className='flex'>product type</div>
+        )}
+      </div>
+      <div className='flex justify-center items-center'>
+        <div className='flex'>
+          <UIButton text={'Add To Cart'} />
+        </div>
+        <div className='flex'>
+          <UIIcon iconType={IconType.LIKE} hasRoute={false} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCheckout;
