a01310b328d30781a666494f14c085fe10c7e7e8
Samuel Oyediran <samuel.oyediran00@gmail.com>
2021-10-24 21:06:49 -0400

    UILink and NavLink for naviagtion

--- src/UIKit/UILink/UILink.tsx
+++ src/UIKit/UILink/UILink.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import ClassNames from 'classnames';
+import { Link, NavLink } from 'react-router-dom';
 
 interface UILinkProps {
   linkTo?: string;
+  isActive?: string;
+  activeClassName?: string;
+  activeStyle?: object;
+  parentClassName?: string;
 }
 
 const UILink: React.FC<UILinkProps> = (props) => {
-  const { linkTo } = props;
-  return <div></div>;
+  const {
+    linkTo,
+    isActive,
+    activeClassName,
+    activeStyle,
+    children,
+    parentClassName,
+  } = props;
+  return (
+    <div>
+      <li className={ClassNames('link__container', parentClassName)}>
+        {isActive ? (
+          <NavLink
+            to={linkTo}
+            activeStyle={activeStyle}
+            activeClassName={activeClassName}
+          >
+            {children}
+          </NavLink>
+        ) : (
+          <Link to={linkTo}>{children}</Link>
+        )}
+      </li>
+    </div>
+  );
 };
+
+export default UILink;
