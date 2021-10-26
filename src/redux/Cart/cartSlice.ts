2fc2b5c3bca76f9efc93d76d209bff34f46d6ade
Samuel Oyediran <samuel.oyediran00@gmail.com>
2021-10-26 12:10:05 -0400

    refactor redux store

--- src/redux/Cart/cartSlice.ts
+++ src/redux/Cart/cartSlice.ts
@@ -1,5 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = { itemCount: 0 };
+
+const cart = (state = initialState, action) => {
+  switch (action.payload) {
+    default:
+      return state;
+  }
+};
+
 export const CartSlice = createSlice({
   name: 'Cart',
   initialState: { product: 0 },
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import CartReducer from './Cart/Cart';
+import CartReducer from './Cart/cartSlice';
 
 const store = configureStore({ reducer: { cart: CartReducer } });
 
