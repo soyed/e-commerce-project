47dac056d738821174a14e9132bfb2d0e299a231
Samuel Oyediran <samuel.oyediran00@gmail.com>
2021-10-30 03:23:21 -0400

    hero section updated

--- src/components/HomePage/Hero-Section/HeroSection.tsx
+++ src/components/HomePage/Hero-Section/HeroSection.tsx
@@ -2,34 +2,46 @@ import React from 'react';
 import { LinkPath, RouteType } from '../../../routes/utils';
 import UILink from '../../../UIKit/UILink/UILink';
 import './HeroSection.scss';
+import womenHero from '../../../assets/home-page-women.jpg';
+import menHero from '../../../assets/homepage-men.jpg';
 interface HeroSectionProps {}
 
 const HeroSection: React.FC<HeroSectionProps> = (props) => {
   const {} = props;
   return (
     <div className='hero-section flex justify-center h-screen-1/2'>
-      <div className='hero-section--container flex justify-evenly w-4/5 bg-red-300'>
-        <div className='hero-section--container-1 flex justify-center items-center'>
-          <UILink
-            linkTo={LinkPath.MEN}
-            linkType={RouteType.COMMERCE}
-            isActive={false}
-          >
-            <button className='hero-section--container-1__btn'>
-              Shop Male
-            </button>
-          </UILink>
+      <div className='hero-section--container  '>
+        <div className='hero-section--container-1'>
+          <div className='container-1__body-1'>
+            <img className='hero--img' src={menHero} alt='men-hero-img' />
+          </div>
+          <div className='container-1__content'>
+            <UILink
+              linkTo={LinkPath.MEN}
+              linkType={RouteType.COMMERCE}
+              isActive={false}
+            >
+              <button className='hero-section__btn'>Shop Male</button>
+            </UILink>
+          </div>
         </div>
-        <div className='hero-section--container-2 flex justify-center items-center'>
-          <UILink
-            linkTo={LinkPath.WOMEN}
-            linkType={RouteType.COMMERCE}
-            isActive={false}
-          >
-            <button className='hero-section--container-2__btn'>
-              Shop Female
-            </button>
-          </UILink>
+        <div className='hero-section--container-2'>
+          <div className='container-2__body-2'>
+            <img
+              className='hero--img'
+              src={womenHero}
+              alt='women-landing-image'
+            />
+          </div>
+          <div className='container-2__content'>
+            <UILink
+              linkTo={LinkPath.WOMEN}
+              linkType={RouteType.COMMERCE}
+              isActive={false}
+            >
+              <button className='hero-section__btn'>Shop Female</button>
+            </UILink>
+          </div>
         </div>
       </div>
     </div>
