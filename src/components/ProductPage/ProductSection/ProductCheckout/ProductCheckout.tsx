3a4b904df1e8a17506f6a9102ef2c16450010452
Samuel Oyediran <samuel.oyediran00@gmail.com>
2021-11-06 14:13:25 -0400

    styling the product page

--- src/components/ProductPage/ProductSection/ProductCheckout/ProductCheckout.tsx
+++ src/components/ProductPage/ProductSection/ProductCheckout/ProductCheckout.tsx
@@ -56,32 +56,47 @@ const ProductCheckout: React.FC<ProductCheckoutProps> = (props) => {
     <div className='product-checkout'>
       <div className='product-checkout__container'>
         <div className='product-checkout__container-1'>
-          <h3>{product.name}</h3>
-          <p>{`Price: $${product.price}`}</p>
+          <div className='flex'>{`${product.ratings} stars`}</div>
         </div>
         <div className='product-checkout__container-2'>
-          <p>{`Color: ${product.color}`}</p>
-          <div className='flex'>{product.ratings}</div>
+          <div className='container-2__1'>
+            <p className='container-2__text-1'>{product.name}</p>
+          </div>
+          <div className='container-2__2'>
+            <p className='container-2__text-2'>{`$${product.price}`}</p>
+          </div>
         </div>
-        <div className='product-checkout__container-3 '>
-          <div className='flex'>
+
+        <div className='product-checkout__container-3'>
+          <p className='container-3__text-1'>Color:</p>
+          <p className='container-3__text-2'>{product.color}</p>
+        </div>
+        <div className='product-checkout__container-4 '>
+          <div className='checkout__section'>
             <UIIcon
               iconType={IconType.DECREMENT}
               hasRoute={false}
               onClickIcon={handleDecrementQuantity}
+              containerClassName='checkout__section--btn-1'
             />
-            <p>{productQuantity}</p>
+            <div className='checkout__section--quantity'>
+              <p>{productQuantity}</p>
+            </div>
             <UIIcon
               iconType={IconType.INCREMENT}
               hasRoute={false}
               onClickIcon={handleIncrementQuantity}
+              containerClassName='checkout__section--btn-2'
             />
           </div>
-          <div className='flex'>
-            <div className='flex'>
+        </div>
+
+        <div className='product-checkout__container-5 '>
+          <div className='checkout__buttons'>
+            <div className='checkout__buttons--1'>
               <UIButton text={'Add To Cart'} onClickButton={handleAddToCart} />
             </div>
-            <div className='flex'>
+            <div className='checkout__buttons--2'>
               <UIIcon
                 iconType={IconType.LIKE}
                 hasRoute={false}
