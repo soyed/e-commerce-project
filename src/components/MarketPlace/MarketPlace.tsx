ed528ddd32e4d91ba2dd3a15d1f191ff4f492639
Samuel Oyediran <samuel.oyediran00@gmail.com>
2021-11-06 18:19:24 -0400

    displaying all marketplace products

--- src/components/MarketPlace/MarketPlace.tsx
+++ src/components/MarketPlace/MarketPlace.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { CartItem } from '../../redux/Cart/utils';
 import { STATUS } from '../../redux/Category/utils';
 import {
   getMarketplaceError,
@@ -7,7 +8,10 @@ import {
   getMarketplaceStatus,
 } from '../../redux/MarketPlace/marketPlaceSelectors';
 import { fetchMarketPlaceProducts } from '../../redux/MarketPlace/utils';
+import { addToWishList } from '../../redux/WishList/wishlistActionCreators';
+import UICard from '../../UIKit/UICard/UICard';
 import UILoadingSpinner from '../../UIKit/UILoadingSpinner/UILoadingSpinner';
+import { DEFAULT_QUANTITY } from '../../utils/utils';
 import { Product } from '../ProductPage/model';
 import './MarketPlace.scss';
 
@@ -23,16 +27,39 @@ const MarketPlace = () => {
   const status: STATUS = useSelector(getMarketplaceStatus);
   const errorMessage: string = useSelector(getMarketplaceError);
 
+  const handleLikeProduct = (product: Product) => {
+    const item: CartItem = {
+      product: product,
+      quantity: DEFAULT_QUANTITY,
+    };
+    // Add to wishlist
+    dispatch(addToWishList(item));
+  };
+
   return (
     <div className='market-place'>
-      {true && (
+      {status === STATUS.FAILED && (
         <div className='market-place__container-1'>
           <UILoadingSpinner />
         </div>
       )}
-
-      <div className='market-place__container-2'></div>
-      <div className='market-place__container-3'></div>
+      <div className='market-place__container-2'>
+        {products?.map((product) => (
+          <UICard
+            key={product.id}
+            src={product.image}
+            alt={product.name}
+            price={product.price}
+            name={product.name}
+            onClickIcon={() => {
+              handleLikeProduct(product);
+            }}
+          />
+        ))}
+      </div>
+      {status === STATUS.FAILED && (
+        <div className='market-place__container-3'>{errorMessage}</div>
+      )}
     </div>
   );
 };
