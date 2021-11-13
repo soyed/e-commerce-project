7bbaafbb5eb45af715da4f2de5cda6d7fdcd8cd7
Samuel Oyediran <samuel.oyediran00@gmail.com>
2021-11-13 01:46:32 -0500

    securing firebase keys

--- src/services/Firebase/firebase-utils.ts
+++ src/services/Firebase/firebase-utils.ts
@@ -2,11 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from './firebase.config';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseApp = initializeApp(firebaseConfig);
+// analytics
+const analytics = getAnalytics(firebaseApp);
 
 const firestore = getFirestore();
 
 const firebaseAuth = getAuth();
 
-export { firestore, firebaseApp, firebaseAuth };
+export { firestore, firebaseApp, firebaseAuth, analytics };
