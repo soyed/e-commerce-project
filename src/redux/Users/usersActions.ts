eca7e6adce6fb0a5ac25f041481ca6dc0741971a
Samuel Oyediran <samuel.oyediran00@gmail.com>
2021-11-16 02:19:45 -0500

    temp fix for .env not loading on netlify

--- src/redux/Users/usersActions.ts
+++ src/redux/Users/usersActions.ts
@@ -3,6 +3,8 @@ const usersActions = {
   SIGN_IN_USER: 'users/signInUser',
   ADD_TO_PURCHASE_HISTORY: 'users/addToPurchaseHistory',
   SIGN_OUT_USER: 'users/signOutUser',
+  USER_STATUS: 'users/userStatus',
+  CURRENT_USER: 'users/currentUser',
 };
 
 export default usersActions;
--- src/services/Firebase/firebase.config.ts
+++ src/services/Firebase/firebase.config.ts
@@ -1,12 +1,12 @@
 import { GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth';
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyBDJMz45rHQJT8DvWnuNe0KGxVNaju5nfk',
+  authDomain: 'e-commerce-ea348.firebaseapp.com',
+  projectId: 'e-commerce-ea348',
+  storageBucket: 'e-commerce-ea348.appspot.com',
+  messagingSenderId: '467960020405',
+  appId: '1:467960020405:web:8350691226a7dc4863edfc',
+  measurementId: 'G-03TEHPW5HJ',
 };
 
 const googleProvider = new GoogleAuthProvider();
@@ -18,3 +18,11 @@ export enum ProviderType {
 }
 
 export { firebaseConfig, googleProvider, facebookProvider };
+
+// apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID,
+//   measurementId: process.env.FIREBASE_MEASUREMENT_ID,
